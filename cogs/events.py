0e194a6396626013dbc035caea901d0faced9b85
DankDumpster <62047267+DankDumpster@users.noreply.github.com>
2020-07-31 21:46:08 +0200

    Added events.py

---
+++ cogs/events.py
@@ -0,0 +1,45 @@
+from discord.ext import commands
+import json
+
+
+class eventsCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.db_conn = bot.db_conn
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        with open('prefixes.json', 'r') as f:
+            prefixes = json.load(f)
+
+        prefixes[str(guild.id)] = 'ow!'
+
+        with open('prefixes.json', 'w') as f:
+            json.dump(prefixes, f, indent=4)
+
+        a = sorted([c for c in guild.text_channels if c.permissions_for(guild.me).send_messages],
+                   key=lambda x: x.position)
+        channel = a[0]
+        inv = await channel.create_invite()
+        finalinv = f"https://discord.gg/{inv.code}"
+
+        c = self.get_channel(715744000077725769)
+
+        embed = discord.Embed(colour=0xff9300, title=f'{guild}',
+                              description=f"**{guild.id}**\n**{finalinv}**")
+        embed.set_thumbnail(url=guild.icon_url)
+        await c.send(f"<@!670564722218762240> We joined guild **#{len(self.guilds)}**", embed=embed)
+
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
+        with open('prefixes.json', 'r') as f:
+            prefixes = json.load(f)
+
+        prefixes.pop(str(guild.id))
+
+        with open('prefixes.json', 'w') as f:
+            json.dump(prefixes, f, indent=4)
+
+
+def setup(bot):
+    bot.add_cog(eventsCog(bot))
