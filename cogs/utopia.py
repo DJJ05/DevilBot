88b90a8e6f2acdc28eabf45bdc21a18269e4caab
Raj Sharma <yrsharma@icloud.com>
2020-09-06 11:35:47 +0100

    wdewdwe

---
+++ cogs/utopia.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+import json, random
+
+class utopiaCog(commands.Cog):
+    """Utopian-only commands"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.db_conn = bot.db_conn
+        self.colour = 0xff9300
+        self.footer = 'Bot developed by DevilJamJar#0001\nWith a lot of help from ♿nizcomix#7532'
+        self.thumb = 'https://styles.redditmedia.com/t5_3el0q/styles/communityIcon_iag4ayvh1eq41.jpg'
+
+    async def cog_check(self, ctx):
+        return ctx.guild.id == 621044091056029696
+
+    @commands.command()
+    async def nominate(self, ctx, message:discord.Message):
+        with open('nominees.json', 'r') as f:
+            nominees = json.load(f)
+        nominees[f'{str(message.clean_content)}∫√∆{str(message.author)}∫√∆{str(message.created_at)}'] = str(message.id)
+        with open('nominees.json', 'w') as f:
+            json.dump(nominees, f, indent=4)
+        await ctx.send(f'{ctx.author.mention}, successfully inserted message into nominations. Use {ctx.prefix}utopiaquote for a random one!')
+
+    @commands.command(aliases=['uq', 'utopiaq', 'uquote'])
+    async def utopiaquote(self, ctx):
+        with open('nominees.json', 'r') as f:
+            nominees = json.load(f)
+
+def setup(bot):
+    bot.add_cog(utopiaCog(bot))
