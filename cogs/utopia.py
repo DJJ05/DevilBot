495fba32ac1ccd799c01ee22f8d08c6de1b58bb8
nizcomix <65248634+niztg@users.noreply.github.com>
2020-09-13 15:33:47 -0400

    Made utopia cog better
    Notes:
    - Fixed horrible `∫√∆` thing.
    - Made `created` an actual datetime object
    - Made the code and imports cleaner
    
    this isn't tested btw
    
    ~ with <3 from niz

--- cogs/utopia.py
+++ cogs/utopia.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
-import json, random
+import json
+import random
+from datetime import datetime as dt
 
 class utopiaCog(commands.Cog):
     """Utopian-only commands"""
@@ -14,6 +16,17 @@ class utopiaCog(commands.Cog):
 
     async def cog_check(self, ctx):
         return ctx.guild.id == 621044091056029696
+    
+    @property
+    def channel(self):
+        return self.bot.get_channel(752472138299998238)
+    
+    async def get_user(self, id: int):
+        try:
+            user = self.bot.get_user(id) or await self.bot.fetch_user(id)
+        except Exception as error:
+            raise error
+        return user
 
     @commands.command()
     async def nominate(self, ctx, message:discord.Message):
@@ -23,13 +36,21 @@ class utopiaCog(commands.Cog):
         if len(message.attachments):
             for i in message.attachments:
                 finmsg+=f'\n{i.url}'
-        nominees[f'{str(finmsg)}∫√∆{str(message.author)}∫√∆{str(message.created_at)}'] = str(message.id)
+        nominees[str(message.id)] = {'author': message.author.id, 'content': str(finmsg), 'created': str(message.created_at), 'url': message.jump_url}
         with open('nominees.json', 'w') as f:
             json.dump(nominees, f, indent=4)
-        channel = self.bot.get_channel(752472138299998238)
         embed=discord.Embed(
             colour = self.colour,
-            description=f'**Message:**\n{str(finmsg)}\n**Author:**\n{str(message.author)}\n**Created At:**\n{str(message.created_at)}\n**Jump:**\n{str(message.jump_url)}'
+            description=f"""
+            **Message:**:
+            {finmsg}
+            **Author:**
+            {str(message.author)}
+            **Created At:**
+            {str(message.created_at)}
+            **Jump:**
+            {str(message.jump_url)}
+            """
         )
         await channel.send(embed=embed)
         await ctx.send(f'{ctx.author.mention}, successfully inserted message into nominations. Use {ctx.prefix}utopiaquote for a random one!')
@@ -39,13 +60,12 @@ class utopiaCog(commands.Cog):
         full=[]
         with open('nominees.json', 'r') as f:
             nominees = json.load(f)
-        for i in nominees:
-            full.append(i)
-        final = random.choice(full)
-        final = final.split('∫√∆')
+        choice = random.choice(*[i[1] for i in nominees.items()])
+        msg = nominees.get(choice)
+        created = dt.strptime(msg['created'], '%Y-%m-%d %H:%M:%S.%f') # an actual datetime object
         embed=discord.Embed(
             colour = self.colour,
-            description=f'**Message:**\n{final[0]}\n**Author:**\n{final[1]}\n**Created At:**\n{final[2]}'
+            description=f'**Message:**\n{msg['content']}\n**Author:**\n{await self.get_user(msg['author'])}\n**Created At:**\n{created.strftime("%B %d, %Y")}'
         )
         await ctx.send(embed=embed)
 
