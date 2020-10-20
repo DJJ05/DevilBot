368226c8c93bfe6c386bf4a5f4a82ef56f143e4f
nizcomix <65248634+niztg@users.noreply.github.com>
2020-10-20 15:15:12 -0400

    removed all of the fluff

--- bot.py
+++ bot.py
@@ -13,10 +13,10 @@ os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True"
 intents = discord.Intents().all()
 
 class Bot(commands.AutoShardedBot):
-    def __init__(self, database_conn, event_loop):
-        super().__init__(command_prefix=self.get_prefix, intents=intents, case_insensitive=True, loop=event_loop,
+    def __init__(self):
+        super().__init__(command_prefix=self.get_prefix, intents=intents, case_insensitive=True,
                          description="")
-        self.db_conn = database_conn
+        self.db_conn = self.loop.run_until_complete(self.init_db())
         self.blacklist = self.initialize_blacklist()
         self.colour = 0xff9300
         self.footer = 'Bot developed by DevilJamJar#0001\nWith a lot of help from ♿nizcomix#7532'
@@ -32,7 +32,7 @@ class Bot(commands.AutoShardedBot):
 
         print('——————————————————————————————')
         for filename in os.listdir('cogs'):
-            if filename.endswith('.py') and filename != 'secrets.py' and filename != 'dagpi.py':
+            if filename.endswith('.py') and filename not in ('secrets.py', 'dagpi.py'):
                 self.load_extension('cogs.{}'.format(filename[:-3]))
                 print(f'{self.tgreen}[LOADED]{self.endc} cogs.{filename}')
         self.load_extension(name='jishaku')
@@ -60,6 +60,14 @@ class Bot(commands.AutoShardedBot):
             activity=discord.Activity(type=5, name="ow!help"))
         print(f'{self.tgreen}Status changed successfully {self.endc}\n——————————————————————————————')
         
+    async def init_db(self):
+        return await asyncpg.create_pool(user=secrets.secrets_pg_user,
+                                                         password=secrets.secrets_pg_password,
+                                                         host=secrets.secrets_pg_host,
+                                                         port=secrets.secrets_pg_port,
+                                                         database=secrets.secrets_pg_database,
+                                                         )
+        
     def initialize_blacklist(self) -> dict:
         with open('blacklist.json', 'r') as f:
             return json.load(f)
@@ -67,44 +75,14 @@ class Bot(commands.AutoShardedBot):
     def run(self):
         super().run(secrets.secrets_token)
 
-class DataBase:
-    db_conn = None
 
-    @staticmethod
-    async def initiate_database() -> bool:
-        try:
-            DataBase.db_conn = await asyncpg.create_pool(user=secrets.secrets_pg_user,
-                                                         password=secrets.secrets_pg_password,
-                                                         host=secrets.secrets_pg_host,
-                                                         port=secrets.secrets_pg_port,
-                                                         database=secrets.secrets_pg_database,
-                                                         )
-            return True
+bot = Bot()
 
-        except Exception as err:
-            print(f'\033[31m Failed to connect to database \033[1;31m', err, '\033[m \n——————————————————————————————')
-            raise
-
-def main():
+@bot.check
+async def check_blacklist(ctx):
+    if str(ctx.author.id) in bot.blacklist['users']:
+        raise commands.CheckFailure(f'You have been blacklisted from using me. Please check you DMs for a reason or contact DevilJamJar#0001 to appeal.')
+    else:
+        return True
     
-    if not DataBase.initiate_database():
-        sys.exit()
-
-    event_loop = asyncio.get_event_loop()
-    if not event_loop.run_until_complete(DataBase.initiate_database()):
-        sys.exit()
-
-    bot = Bot(database_conn=DataBase.db_conn, event_loop=event_loop)
-
-    @bot.check
-    async def check_blacklist(ctx):
-        if str(ctx.author.id) in bot.blacklist['users']:
-            raise commands.CheckFailure(f'You have been blacklisted from using me. Please check you DMs for a reason or contact DevilJamJar#0001 to appeal.')
-        else:
-            return True
-
-    # bot.remove_command('help')
-    bot.run()
-
-if __name__ == '__main__':
-    main()
+bot.run()
